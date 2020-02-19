494b6a2aef073ccdb5e21db429eaac073d70c991
alexandre jamarco <alexandre.j@gmail.com>
2020-02-19 11:17:33 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
+    get '/newteam' do
+        erb :newteam
+      end
+    
+      post '/team' do
+        @name = params[:name]
+        @coach = params[:coach]
+        @pointg = params[:pg]
+        @shootingg = params[:sg]
+        @powerf = params[:pf]
+        @smallf = params[:sf]
+        @center = params[:c]
+        erb :team
+      end
 
 end
